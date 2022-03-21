7981619a21693f6f59093394a7af0921a8319e1a
DayoOj <ojouyedayo@gmail.com>
2022-03-21 09:20:31 -0700

    My fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ 8
+ *Return: 0 after printingthe function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
