ae70b96b487751ac5e6488bfad3da4a20c5785d6
DayoOj <ojouyedayo@gmail.com>
2022-03-22 13:32:47 -0700

    My commit message

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,7 +12,7 @@ char *start_c, *end_c, c;
 int i, count;
 int length = 0;
 
-for (i = 0; s([i]; i++)
+for (i = 0; s[i]; i++)
 {
 length++;
 }
@@ -31,9 +31,11 @@ end_c++;
 for (i = 0; i < count / 2; i++)
 {
 
+
 c = *end_c;
 *end_c = *start_c;
 *start_c = c;
+
 
 start_c++;
 end_c--;
