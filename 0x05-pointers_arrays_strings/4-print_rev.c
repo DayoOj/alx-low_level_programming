5cd5d43ed45956fe00934f5fc3f8e2b8f6a841f4
DayoOj <ojouyedayo@gmail.com>
2022-03-22 13:03:09 -0700

    My commit message

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,12 +15,10 @@ while (s[i] != '\0')
 i++;
 }
 
-for (i = i - 1; i >= 0; i--)
+for (i = i - 1 ; i >= 0; i--)
 {
 _putchar (s[i]);
 }
 
 _putchar ('\n');
 }
-git add .
-git commit -m 'My commit message'
