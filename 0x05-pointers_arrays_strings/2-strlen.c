0ca3e81a64ab16d77b4cdd5095036c711731a1d6
DayoOj <ojouyedayo@gmail.com>
2022-03-22 08:43:09 -0700

    My third commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strien -lenght of a string
+ *@s: A pointer to an int that will be changed/updated
+ *
+ *Return: void that means our answer is collect 
+ */
+
+int _strien(char *s)
+{
+int u;
+
+u = 0;
+while (s[u] != '
+{
+u++;
+}
+return (u);
+}
