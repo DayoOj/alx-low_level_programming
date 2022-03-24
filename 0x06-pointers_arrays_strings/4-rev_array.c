cc580f94ccde99fb11285d6e4030cf235d488f56
DayoOj <ojouyedayo@gmail.com>
2022-03-24 06:19:57 -0700

    My commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reverse the content of an array of an integers
+ * reverse_array - reverses the content of an array of an integers
  * @a: an array of integers
  * @n: number of elements of the array
  * Return: return void
